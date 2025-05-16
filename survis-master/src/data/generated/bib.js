dbfd705091bcbff89cc44b2485a20d522400f6e1
YongQi <89056164+kyq1025@users.noreply.github.com>
2025-05-16 13:49:50 +0100

    Add files via upload

--- survis-master/src/data/generated/bib.js
+++ survis-master/src/data/generated/bib.js
@@ -13,5 +13,113 @@
         "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
         "volume": "22",
         "year": "2016"
+    },
+    "cui2025practice": {
+        "author": "T. Cui and Y. Shi and J. Wang and R. Ding and J. Li and K. Li",
+        "doi": "10.1007/s40747-024-01725-4",
+        "journal": "Complex \\& Intelligent Systems",
+        "keywords": "application_case_study",
+        "number": "136",
+        "title": "Practice of an improved many-objective route optimization algorithm in a multimodal transportation case under uncertain demand",
+        "type": "article",
+        "volume": "11",
+        "year": "2025"
+    },
+    "li2021effective": {
+        "author": "Z. Li and Y. Liu and Z. Yang",
+        "doi": "10.1016/j.tre.2021.102408",
+        "journal": "Transportation Research Part E: Logistics and Transportation Review",
+        "keywords": "heuristic_algorithm",
+        "pages": "102408",
+        "title": "An effective kernel search and dynamic programming hybrid heuristic for a multimodal transportation planning problem with order consolidation",
+        "type": "article",
+        "volume": "152",
+        "year": "2021"
+    },
+    "li2024many": {
+        "author": "Z. Li and T. Li and V. Smith and J. Bilmes and T. Zhou",
+        "doi": "10.48550/arXiv.2403.04099",
+        "howpublished": "arXiv preprint arXiv:2403.04099",
+        "keywords": "many-objective_model",
+        "title": "Many-Objective Multi-Solution Transport",
+        "type": "misc",
+        "year": "2024"
+    },
+    "maneengam2023multi": {
+        "author": "A. Maneengam",
+        "doi": "10.3390/su151512066",
+        "journal": "Sustainability",
+        "keywords": "multi-objective_optimisation",
+        "pages": "12066",
+        "title": "Multi-Objective Optimization of the Multimodal Routing Problem Using the Adaptive $\\varepsilon$-Constraint Method and Modified TOPSIS with the D-CRITIC Method",
+        "type": "article",
+        "volume": "15",
+        "year": "2023"
+    },
+    "matei2021selective": {
+        "author": "O. Matei and E. Rudolf and C. Pintea",
+        "doi": "10.48550/arXiv.2103.15555",
+        "journal": "Informatica",
+        "keywords": "survey",
+        "number": "2",
+        "pages": "371--396",
+        "title": "Selective Survey: Most Efficient Models and Solvers for Integrative Multimodal Transport",
+        "type": "article",
+        "volume": "32",
+        "year": "2021"
+    },
+    "park2022multi": {
+        "author": "S. J. Park and S. Kang and Y. Byon and S. Kho",
+        "doi": "10.1155/2022/5887985",
+        "journal": "Journal of Advanced Transportation",
+        "keywords": "transit_equity",
+        "pages": "5887985",
+        "title": "Multi-Objective Approach to the Transit Network Design Problem with Variable Demand considering Transit Equity",
+        "type": "article",
+        "volume": "2022",
+        "year": "2022"
+    },
+    "peng2023multi": {
+        "author": "Y. Peng and S. H. Gao and D. Yu and Y. P. Xiao and Y. J. Luo",
+        "doi": "10.1051/ro/2023090",
+        "journal": "RAIRO Operations Research",
+        "keywords": "data-driven_model",
+        "pages": "1745--1765",
+        "title": "Multi-Objective Optimisation for Multimodal Transportation Routing Problem with Stochastic Transportation Time based on Data-driven Approaches",
+        "type": "article",
+        "volume": "57",
+        "year": "2023"
+    },
+    "udomwannakhet2018review": {
+        "address": "Bangkok, Thailand",
+        "author": "J. Udomwannakhet and P. Vajarodaya and S. Manicho and K. Kaewfak and J. B. Ruiz and V. Ammarapala",
+        "booktitle": "2018 5th International Conference on Business and Industrial Research (ICBIR)",
+        "doi": "10.1109/ICBIR.2018.8391217",
+        "keywords": "review",
+        "pages": "333--338",
+        "title": "A Review of Multimodal Transportation Optimization Model",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "zhang2024enhancing": {
+        "author": "W. Zhang and G. Xiao and M. Gen and H. Geng and X. Wang and M. Deng and G. Zhang",
+        "doi": "10.3389/fieng.2024.1337174",
+        "journal": "Industrial Informatics",
+        "keywords": "survey",
+        "title": "Enhancing multi-objective evolutionary algorithms with machine learning for scheduling problems: recent advances and survey",
+        "type": "article",
+        "volume": "2",
+        "year": "2024"
+    },
+    "zhang2024multimodal": {
+        "author": "T. Zhang and J. Cheng and Y. Zou",
+        "doi": "10.1007/s40747-023-01308-9",
+        "journal": "Complex \\& Intelligent Systems",
+        "keywords": "algorithm",
+        "pages": "3133--3152",
+        "title": "Multimodal transportation routing optimization based on multi-objective Q-learning under time uncertainty",
+        "type": "article",
+        "volume": "10",
+        "year": "2024"
     }
 };
